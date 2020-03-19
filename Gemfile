0bd07c112bd38da3faf4c9b0e10fd4405c183511
bren2102 <brb.yg.21@gmail.com>
2020-03-19 11:30:55 -0500

    Finish layout and routes

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'rails',        '5.2'
+gem 'bootstrap-sass', '3.3.7'
 gem 'puma',         '3.9.1'
 gem 'sass-rails',   '5.0.6'
 gem 'uglifier',     '3.2.0'
@@ -9,6 +10,7 @@ gem 'jquery-rails', '4.3.1'
 gem 'turbolinks',   '5.0.1'
 gem 'jbuilder',     '2.7.0'
 gem 'bootsnap'
+
 
 group :development, :test do
   gem 'sqlite3', '1.3.13'
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/new'
   root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get  'static_pages/about'
+  get '/home', to: 'static_pages#home'
+  get '/help', to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+  get '/contact', to: 'static_pages#contact'
+  get '/signup', to: 'users#new'
+
 end

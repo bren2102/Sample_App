356de4571de0808abeaa832dd05d9f7a3b6ca737
bren2102 <brb.yg.21@gmail.com>
2020-03-25 11:15:35 -0500

    FInish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/new'
   root 'static_pages#home'
   get '/home', to: 'static_pages#home'
   get '/help', to: 'static_pages#help'
@@ -7,5 +6,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  resources :users
 
 end
---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,16 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name:  "",
+                                         email: "user@invalid",
+                                         password:              "foo",
+                                         password_confirmation: "bar" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.FILL_IN
+  end
+end

bc434f407ffc16162870be2b709ba85a7e418827
bren2102 <brb.yg.21@gmail.com>
2020-03-27 16:31:13 -0500

    Implement basic login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
-  def hello
-    render html: "hello, world!"
-  end
+  
+  protect_from_forgery with: :exception
+  include SessionsHelper
+
 end
